8198d07c2ab0078869c67094cc9e714b0a193b10
ara.t.howard <ara.t.howard@gmail.com>
2010-04-08 14:37:42 -0600

    clean up tests

--- test/hashish.rb
+++ test/hashish.rb
@@ -1,4 +1,7 @@
+require File.join(File.dirname(__FILE__), 'testing')
+
 require 'hashish'
+
 
 Testing Hashish do
 
@@ -195,77 +198,3 @@ Testing Hashish do
     assert{ errors[global].nil? }
   end
 end
-
-
-
-
-
-
-
-
-
-BEGIN {
-  require 'test/unit'
-  STDOUT.sync = true
-  $:.unshift 'lib'
-  $:.unshift '../lib'
-  $:.unshift '.'
-
-  def Testing(*args, &block)
-    Class.new(Test::Unit::TestCase) do
-      def self.slug_for(*args)
-        string = args.flatten.compact.join('-')
-        words = string.to_s.scan(%r/\w+/)
-        words.map!{|word| word.gsub %r/[^0-9a-zA-Z_-]/, ''}
-        words.delete_if{|word| word.nil? or word.strip.empty?}
-        words.join('-').downcase
-      end
-
-      @@testing_subclass_count = 0 unless defined?(@@testing_subclass_count) 
-      @@testing_subclass_count += 1
-      slug = slug_for(*args).gsub(%r/-/,'_')
-      name = ['TESTING', '%03d' % @@testing_subclass_count, slug].delete_if{|part| part.empty?}.join('_')
-      name = name.upcase!
-      const_set(:Name, name)
-      def self.name() const_get(:Name) end
-
-      def self.testno()
-        '%05d' % (@testno ||= 0)
-      ensure
-        @testno += 1
-      end
-
-      def self.testing(*args, &block)
-        method = ["test", testno, slug_for(*args)].delete_if{|part| part.empty?}.join('_')
-        define_method("test_#{ testno }_#{ slug_for(*args) }", &block)
-      end
-
-      alias_method '__assert__', 'assert'
-
-      def assert(*args, &block)
-        if block
-          label = "assert(#{ args.join(' ') })"
-          result = nil
-          assert_nothing_raised{ result = block.call }
-          __assert__(result, label)
-          result
-        else
-          result = args.shift
-          label = "assert(#{ args.join(' ') })"
-          __assert__(result, label)
-          result
-        end
-      end
-
-      def subclass_of exception
-        class << exception
-          def ==(other) super or self > other end
-        end
-        exception
-      end
-
-      module_eval &block
-      self
-    end
-  end
-}
